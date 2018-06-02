3fa10fa6e7b6297934dc50e2545576e3882acaa1
Alara <0931871@hr.nl>
2018-06-02 15:30:11 +0200

    Comments Toegevoegd

--- CMTTHE04-Week4-oefening1/dev/game.ts
+++ CMTTHE04-Week4-oefening1/dev/game.ts
@@ -6,29 +6,38 @@ class Game {
     private paddle:Paddle
 
     constructor() {
+        //                       xp, up, down
         this.paddle = new Paddle(20, 87, 83)
         
+        //5 ballen in het spel.
         for (let i = 0; i < 5; i++) {
             this.balls.push(new Ball())
         }
 
+        //Speel gameloop functie af.
         this.gameLoop()        
     }
     
     private gameLoop():void{
         for (let b of this.balls) {
+            //Checkt of paddle en bal elkaar raken
             if (this.checkCollision(b.getRectangle(), this.paddle.getRectangle())) {
+                //Schiet bal terug
                 b.hitPaddle()
             }
 
+            //Update bal
             b.update()
         }
 
+        //update paddle
         this.paddle.update()
         
+        //update gameloop 60x per seconde
         requestAnimationFrame(() => this.gameLoop())
     }
 
+    //Botsing paddle & bal
     private checkCollision(a: ClientRect, b: ClientRect) {
         return (a.left <= b.right &&
             b.left <= a.right &&
@@ -38,5 +47,6 @@ class Game {
     
 } 
 
-
+//Laat venster = new game
 window.addEventListener("load", () => new Game())
+
--- CMTTHE04-Week4-oefening1/dev/paddle.ts
+++ CMTTHE04-Week4-oefening1/dev/paddle.ts
@@ -12,29 +12,32 @@ class Paddle {
     private y : number
     
     constructor(xp:number, up:number, down:number) {
+        //Creer paddle
         this.div = document.createElement("paddle")
         document.body.appendChild(this.div)
         
-        this.upkey   = up
-        this.downkey = down
+        //Deze waarden staan in game.ts
+        this.upkey   = up       //87 --> W
+        this.downkey = down     //83 --> S
         
-        this.x      = xp
+        this.x      = xp        //20
         this.y      = 200
         
         window.addEventListener("keydown", (e: KeyboardEvent) => this.onKeyDown(e))
         window.addEventListener("keyup", (e: KeyboardEvent) => this.onKeyUp(e))
     }
-
+    //Bind paddle aan rectangle
     public getRectangle() {
         return this.div.getBoundingClientRect()
     }
 
+    //Control toetsen
     private onKeyDown(e: KeyboardEvent): void {
         switch (e.keyCode) {
-            case this.upkey:
+            case this.upkey:            //W
                 this.upSpeed = 5
                 break
-            case this.downkey:
+            case this.downkey:          //S
                 this.downSpeed = 5
                 break
         }
@@ -42,22 +45,32 @@ class Paddle {
 
     private onKeyUp(e: KeyboardEvent): void {
         switch (e.keyCode) {
-            case this.upkey:
+            case this.upkey:            //W
                 this.upSpeed = 0
                 break
-            case this.downkey:
+            case this.downkey:          //S
                 this.downSpeed = 0
                 break
         }
     }
 
+    //Update paddle
     public update() {
-        let newY = this.y - this.upSpeed + this.downSpeed
+
+        //       = 200    -  5/0         +     5/0
+        let newY = this.y - this.upSpeed + this.downSpeed               //newY geeft nieuwe positie weer
 
         // als de paddle binnen beeld blijft, dan ook echt updaten
-        if (newY > 0 && newY + 100 < window.innerHeight) this.y = newY
+        if (newY > 0 && newY + 100 < window.innerHeight)                //Als niet true, speel rest van code niet af;
+        this.y = newY
 
+        //Beweeg paddle.
         this.div.style.transform = `translate(${this.x}px, ${this.y}px)`
     }
     
 }
+
+
+
+
+
