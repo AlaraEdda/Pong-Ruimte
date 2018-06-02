aea8f44f959b682049c8917df77943ecf88ee649
Alara <0931871@hr.nl>
2018-06-02 16:55:53 +0200

    Overgenomen
    Overgenomen zoals opdracht zegt

--- CMTTHE04-Week4-oefening1/dev/playscreen.ts
+++ CMTTHE04-Week4-oefening1/dev/playscreen.ts
@@ -1,14 +1,43 @@
 class PlayScreen {
 
     game:Game
+    private balls: Ball[] = []
+    private paddle:Paddle
 
     constructor(g:Game) {
         this.game = g
+        //                       xp, up, down
+        this.paddle = new Paddle(20, 87, 83)
+
+        //5 ballen in het spel.
+        for (let i = 0; i < 5; i++) {
+            this.balls.push(new Ball())
+        }
     }
 
     public update(): void {
+        for (let b of this.balls) {
+            //Checkt of paddle en bal elkaar raken
+            if (this.checkCollision(b.getRectangle(), this.paddle.getRectangle())) {
+                //Schiet bal terug
+                b.hitPaddle()
+            }
+
+            //Update bal
+            b.update()
+        }
+
+        //update paddle
+        this.paddle.update()
         
     }
 
+    //Botsing paddle & bal
+    private checkCollision(a: ClientRect, b: ClientRect) {
+        return (a.left <= b.right &&
+            b.left <= a.right &&
+            a.top <= b.bottom &&
+            b.top <= a.bottom)
+    }
 
 }
--- CMTTHE04-Week4-oefening1/docs/js/main.js
+++ CMTTHE04-Week4-oefening1/docs/js/main.js
@@ -115,9 +115,28 @@ var GameOverScreen = (function () {
 }());
 var PlayScreen = (function () {
     function PlayScreen(g) {
+        this.balls = [];
         this.game = g;
+        this.paddle = new Paddle(20, 87, 83);
+        for (var i = 0; i < 5; i++) {
+            this.balls.push(new Ball());
+        }
     }
     PlayScreen.prototype.update = function () {
+        for (var _i = 0, _a = this.balls; _i < _a.length; _i++) {
+            var b = _a[_i];
+            if (this.checkCollision(b.getRectangle(), this.paddle.getRectangle())) {
+                b.hitPaddle();
+            }
+            b.update();
+        }
+        this.paddle.update();
+    };
+    PlayScreen.prototype.checkCollision = function (a, b) {
+        return (a.left <= b.right &&
+            b.left <= a.right &&
+            a.top <= b.bottom &&
+            b.top <= a.bottom);
     };
     return PlayScreen;
 }());
